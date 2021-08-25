715f3a52e92e1d75de4dd829b99f13883282f353
satyajeetsj <satyajeetsj@gmail.com>
2021-08-25 16:14:47 +0530

    autoconst added

--- src/main/java/com/generic/AutoConstants.java
+++ src/main/java/com/generic/AutoConstants.java
@@ -1,8 +1,9 @@
 package com.generic;
 
 public interface AutoConstants {
+	public static String DirPath = System.getProperty("user.dir");
 
-	String prop ="../VTIGER_prac/commonProp.properties";
+	String p ="../VTIGER_prac/commonProp.properties";
 	String JSONpath="../VTIGER_prac/commonJSON.json";
 	String XMLpath="../VTIGER_prac/commonXML.xml";
 	String Excelpath="../VTIGER_prac/Vtiger_Excel.xlsx";
