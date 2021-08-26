f92b421cc32cace33aa4f46362aa42dd0da1b2e0
satyajeetsj <satyajeetsj@gmail.com>
2021-08-26 13:00:47 +0530

    thread

--- src/main/java/com/generic/Base_Class.java
+++ src/main/java/com/generic/Base_Class.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 
 import com.ObjectRepo.HomePage;
 import com.ObjectRepo.Loginpage;
